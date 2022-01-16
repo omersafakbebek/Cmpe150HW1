381ea9c2f1c626a91521339406b419c77d9736e6
Omer Safak Bebek <75090441+omersafakbebek@users.noreply.github.com>
2022-01-16 19:30:35 +0300

    Add files via upload

---
+++ Homework1.py
@@ -0,0 +1,12 @@
+temp_celcius=5
+
+weight=60
+
+height=100
+
+##############DO_NOT_EDIT_ANYTHING_ABOVE_THIS_LINE ##########
+print((temp_celcius/100)*180+32)
+bmi = (weight/(height/100)**2)
+print(bmi)
+##############DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE##########
+
